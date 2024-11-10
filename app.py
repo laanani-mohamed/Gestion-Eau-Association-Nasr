f3fba606b22da1721a71502fabc1ab1d97f6e768
laanani-mohamed <laanani.med13@gmail.com>
2024-11-10 13:07:25 +0100

    2 modification
    jut for checking

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ option = st.selectbox("Choisissez une option :", [
     "Charge Maintenance"
 ])
 
-# Fonction pour vérifier si tous les champs sont remplis
+# suppresion de modification
 def champs_remplis(*args):
     return all(arg not in ("", None) for arg in args)
 
